4c7a09f99e4f1dc20c95f8b89f7863f73b309817
Abhinavbhatt2 <abhinavbhatt2024@gmail.com>
2025-06-25 13:54:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -47,14 +47,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navLinks = document.getElementById('nav-links');
-
-    if (menuToggle && navLinks) {
-        menuToggle.addEventListener('click', function() {
-            // Toggles the 'active' class to show/hide the menu
-            navLinks.classList.toggle('active');
-        });
-    }
-});
